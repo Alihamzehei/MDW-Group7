6f46078a36905bd80f64a4a0d1bfaf2b32aa54b2
joy513 <joy.wsh@gmail.com>
2014-11-26 20:18:22 +0100

    StartApp

---
+++ MDW_GuessNumberGame/MDW_GuessNumberGame/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MDW_GuessNumberGame
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Game : IGame
+    {
+        private List<Player> playerlist;
+        private Player player1;
+        private Player player2;
+        public List<Player> GetPlayerList()
+        {
+            return playerlist;
+        }
+
+        public void StartGame(Player p1,Player p2)
+        { }//if InvitePlayer() is true put two player in the game
+        public void CountGuessTime()
+        {
+        }
+
+        public bool InvitePlayer(Player p1,Player p2) { return false; }
+
+       
+       
+    }
+}
---
+++ MDW_GuessNumberGame/MDW_GuessNumberGame/IGame.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MDW_GuessNumberGame
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [DataContract]
+    public class Player
+    {
+        string userName;
+        string passWord;
+
+        public Player(string userName, string passWord)
+        {
+        }
+
+        [DataMember]
+        public string UserName
+        { }
+        [DataMember]
+        public string PassWord
+        { }
+
+    }
+    [DataContract]
+    public class Number
+    {
+        private int[] arrayNumber;
+        
+
+        public Number(int[] arrayNumber, int arrayLength)
+        {
+            
+            
+        }
+
+        public void CreateGuessNumber()
+        {
+            for(int i=0; i<4;i++)
+            {
+                
+            }
+        }
+        public bool CheckGuessNumber()
+        { return false; }
+
+    }
+    [ServiceContract]
+    public interface IGame
+    {
+        [OperationContract]
+        void StartGame(Player p1,Player p2);
+        [OperationContract]
+        void CountGuessTime();
+        [OperationContract]
+        bool InvitePlayer(Player p1, Player p2);
+    }
+    [ServiceContract]
+    public interface IGameCallBack
+    {
+        
+        [OperationContract]
+        void SendMessage(Player p1, string message);
+
+        
+    }
+
+    
+}
