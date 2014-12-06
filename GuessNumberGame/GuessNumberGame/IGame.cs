52c53d277747a4cd325aa449edbb582d5c046f66
apmxwow <apmxwow@gmail.com>
2014-12-06 17:11:16 +0100

    IGame

--- GuessNumberGame/GuessNumberGame/IGame.cs
+++ GuessNumberGame/GuessNumberGame/IGame.cs
@@ -6,8 +6,33 @@ using System.ServiceModel;
 using System.Text;
 
 namespace Server
-{   
-    [ServiceContract]
+{
+    [DataMember]
+    public class Number
+    {
+        private int[] arrayNumber;
+
+
+        public Number(int[] arrayNumber, int arrayLength)
+        {
+
+
+        }
+
+        public void CreateGuessNumber()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+
+            }
+        }
+        public bool CheckGuessNumber()
+        { return false; }
+
+    }
+
+
+    [ServiceContract(Namespace = "Server", CallbackContract = typeof(IGameCallBack))]
     public interface IGame
     {
         [OperationContract]
@@ -22,10 +47,15 @@ namespace Server
         [OperationContract]
         bool quitGame(Player player);
 
-        
-        
-        
-
         // TODO: Add your service operations here
+    }
+
+    public interface IGameCallBack
+    {
+
+        [OperationContract]
+        void SendMessage(Player p1, string message);
+
+
     }
 }
