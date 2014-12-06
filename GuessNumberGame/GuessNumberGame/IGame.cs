4c6aacd4a37481b42c802cabb67c04f11ae0d76e
apmxwow <apmxwow@gmail.com>
2014-12-06 16:59:19 +0100

    IGame

--- GuessNumberGame/GuessNumberGame/IGame.cs
+++ GuessNumberGame/GuessNumberGame/IGame.cs
@@ -13,14 +13,12 @@ namespace Server
         [OperationContract]
         bool guessNumber(int[] number);
 
-        [OperationContract]
-        void StartGame(Player p1, Player p2);
+       
 
         [OperationContract]
         void CountGuessTime();
 
-        [OperationContract]
-        bool InvitePlayer(Player p1, Player p2);
+        
         
 
         // TODO: Add your service operations here
