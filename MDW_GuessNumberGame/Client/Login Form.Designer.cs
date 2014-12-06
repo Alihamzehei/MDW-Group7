b85e6ecbcb1ef5779d9e55ed4fb5c8bb88546f97
Alihamzehei <alihamzehei@gmail.com>
2014-12-06 12:04:21 +0100

    login
    login

--- MDW_GuessNumberGame/Client/Login Form.Designer.cs
+++ MDW_GuessNumberGame/Client/Login Form.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Client
 {
-    partial class Login_Form
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,11 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.bt_login = new System.Windows.Forms.Button();
+            this.bt_Cancel = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(85, 32);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(100, 20);
+            this.tb_username.TabIndex = 2;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(85, 67);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(100, 20);
+            this.tb_password.TabIndex = 3;
+            // 
+            // bt_login
+            // 
+            this.bt_login.Location = new System.Drawing.Point(85, 117);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(75, 23);
+            this.bt_login.TabIndex = 4;
+            this.bt_login.Text = "Login";
+            this.bt_login.UseVisualStyleBackColor = true;
+            // 
+            // bt_Cancel
+            // 
+            this.bt_Cancel.Location = new System.Drawing.Point(179, 117);
+            this.bt_Cancel.Name = "bt_Cancel";
+            this.bt_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.bt_Cancel.TabIndex = 5;
+            this.bt_Cancel.Text = "Cancel";
+            this.bt_Cancel.UseVisualStyleBackColor = true;
+            this.bt_Cancel.Click += new System.EventHandler(this.bt_Cancel_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(44, 160);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(230, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Please click here if you have not registered yet!";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 222);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.bt_Cancel);
+            this.Controls.Add(this.bt_login);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
             this.Text = "Login_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Button bt_login;
+        private System.Windows.Forms.Button bt_Cancel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
