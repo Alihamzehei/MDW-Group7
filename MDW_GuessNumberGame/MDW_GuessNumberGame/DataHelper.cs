f6a33cce65e04883cd039481226890f116b4efb3
joy513 <joy.wsh@gmail.com>
2014-12-06 12:37:09 +0100

    data

---
+++ MDW_GuessNumberGame/MDW_GuessNumberGame/DataHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace MDW_GuessNumberGame
+{
+    class DataHelper
+    {
+        private OleDbConnection connection;
+        public DataHelper()
+        {
+            String provider = "Provider=Microsoft.ACE.OLEDB.12.0";
+            String databaseInfo = "Data Source=" + @"C:\Users\Joy\Documents\GitHub\MDW-Group7\MDW_GuessNumberGame";
+            String connectionInfo = provider + ";" + databaseInfo;
+            connection = new OleDbConnection(connectionInfo);
+        }
+
+
+    }
+}
