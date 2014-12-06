782673438f7601d6fef27167de4e43a5a4ed0bc9
apmxwow <apmxwow@gmail.com>
2014-12-06 16:48:38 +0100

    new IGame

--- GuessNumberGame/GuessNumberGame/IGame.cs
+++ GuessNumberGame/GuessNumberGame/IGame.cs
@@ -4,10 +4,25 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-namespace Server
-{
-    class IGame
-    {
 
+namespace Server
+{   
+    [ServiceContract]
+    public interface IGame
+    {
+        [OperationContract]
+        bool guessNumber(int[] number);
+
+        [OperationContract]
+        void StartGame(Player p1, Player p2);
+
+        [OperationContract]
+        void CountGuessTime();
+
+        [OperationContract]
+        bool InvitePlayer(Player p1, Player p2);
+        
+
+        // TODO: Add your service operations here
     }
 }
