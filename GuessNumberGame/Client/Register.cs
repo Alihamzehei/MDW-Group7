00484b93e59d9d9b5545657b491c35626474056b
apmxwow <apmxwow@gmail.com>
2014-12-08 14:54:47 +0100

    Register

--- GuessNumberGame/Client/Register.cs
+++ GuessNumberGame/Client/Register.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.ServiceModel;
+using Server;
 
 namespace Client
-{
-    public partial class Register : Form
+{   
+    public partial class Register : Form,ServicePortal.IPortal
     {
+        private ServicePortal.IPortal proxy;
         public Register()
         {
             InitializeComponent();
+            InstanceContext context = new InstanceContext(this);
+            proxy = new ServicePortal.PortalClient(context);
         }
 
         private void Register_Load(object sender, EventArgs e)
@@ -27,6 +32,16 @@ namespace Client
             String userName = this.tb_username.ToString();
             String passWord = this.tb_password.ToString();
             String repassWord = this.tb_repassword.ToString();
+            if (passWord == repassWord)
+            {
+                Player player = new Player(userName,passWord);
+                this.label_error_message.Visible = false;
+                proxy.UserRegister(player);
+            }
+            else {
+                this.label_error_message.Visible = true;
+            }
+            
         }
     }
 }
